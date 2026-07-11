88b1ff40cdf86de423f95e8f4c6c015231aba977
Rasheed124 <rashdevatrealglobe@gmail.com>
2026-07-11 08:11:41 +0100

    added delivery option functionality

--- data/cart.js
+++ data/cart.js
@@ -1,15 +1,25 @@
 import { products } from "./products.js";
 
-export const cart = [
-  {
-    productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-    quantity: 2,
-  },
-  {
-    productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-    quantity: 2,
-  },
-];
+export let cart = JSON.parse(localStorage.getItem("cart"));
+
+if (!cart) {
+  cart = [
+    {
+      productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+      quantity: 1,
+      deliveryOptionId: "1",
+    },
+    {
+      productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+      quantity: 2,
+      deliveryOptionId: "2",
+    },
+  ];
+}
+
+function saveToLocalStorage() {
+  localStorage.setItem("cart", JSON.stringify(cart));
+}
 
 export function addToCart(productId) {
   let matchItem;
@@ -25,10 +35,13 @@ export function addToCart(productId) {
     cart.push({
       productId: productId,
       quantity: 1,
+      deliveryOptionId: "1",
     });
   }
 
   console.log(cart);
+
+  saveToLocalStorage();
 }
 
 export function updateCartQuanity() {
@@ -39,6 +52,8 @@ export function updateCartQuanity() {
   });
 
   document.querySelector(".cart-quantity").innerHTML = cartQuantity;
+
+  saveToLocalStorage();
 }
 
 export function removeFromCart(productCartId) {
@@ -50,5 +65,19 @@ export function removeFromCart(productCartId) {
     }
   });
 
-  return newCart;
+  cart = newCart;
+  saveToLocalStorage();
+}
+
+export function updateDeliveryOption(productId, deliveryOptionId) {
+  let matchItem;
+  cart.forEach((item) => {
+    if (productId === item.productId) {
+      matchItem = item;
+    }
+  });
+
+  matchItem.deliveryOptionId = deliveryOptionId;
+
+  saveToLocalStorage();
 }
---
+++ data/deliveryOptions.js
@@ -0,0 +1,19 @@
+export const deliveryOptions = [
+  {
+    id: "1",
+    deliveryDays: 7,
+    priceCents: 300,
+  },
+
+  {
+    id: "2",
+    deliveryDays: 5,
+    priceCents: 499,
+  },
+
+  {
+    id: "3",
+    deliveryDays: 7,
+    priceCents: 4000,
+  },
+];
